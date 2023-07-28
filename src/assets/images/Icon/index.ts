6b374bc21ed707478da7cd316f0b75646aa274da
LeeSeungHun <dos07008@naver.com>
2023-07-28 20:55:51 +0900

    chore: 이미지 이름 변경

--- src/assets/images/Icon/index.ts
+++ src/assets/images/Icon/index.ts
@@ -1,3 +1,4 @@
+import okIcon from "./ok-button.png";
 import chatIcon from "./chat-icon.png";
 import createIcon from "./create-icon.png";
 import enrollIcon from "./enroll-icon.png";
@@ -6,6 +7,8 @@ import homeIcon from "./home-icon.png";
 import profileIcon from "./profile-icon.png";
 import voiceIcon from "./voice-icon.png";
 import signInIcon from "./가입 로고.png";
+import inputClearButton from "./input-clear-button.png";
+import noIcon from "./no-button.png";
 
 const Images = {
   chatIcon,
@@ -16,6 +19,9 @@ const Images = {
   profileIcon,
   voiceIcon,
   signInIcon,
+  inputClearButton,
+  noIcon,
+  okIcon,
 };
 
 export default Images;
