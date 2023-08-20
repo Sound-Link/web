64c0387ae909e982b8f8f94ea258d2b5a7346433
LeeSeungHun <dos07008@naver.com>
2023-08-20 15:33:49 +0900

    chore: socket 테스트 해보기

--- src/pages/Meeting/Detail/index.tsx
+++ src/pages/Meeting/Detail/index.tsx
@@ -1,6 +1,22 @@
-import { Center } from "@chakra-ui/react";
+import { Button, Center } from "@chakra-ui/react";
+import { useEffect, useRef } from "react";
 
 const MeetingDetail = () => {
+  const socketRef = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    const ws = new WebSocket("wss://socketsbay.com/wss/v2/1/demo/");
+    socketRef.current = ws;
+    ws.onmessage = e => {
+      console.log(e.data);
+    };
+  }, []);
+
+  const handleSendMessage = () => {
+    if (!socketRef.current) return;
+    socketRef.current?.send("testtest12312");
+  };
+
   return (
     <Center
       position="relative"
@@ -10,6 +26,7 @@ const MeetingDetail = () => {
       fontWeight={700}
     >
       Meeting History Detail
+      <Button onClick={handleSendMessage}>메세지 보내기</Button>
     </Center>
   );
 };
